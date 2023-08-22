7f4b69728f6be96d06548676549846986e521368
YarpSue <yarp_sue@163.com>
2023-08-22 15:55:48 +0800

    algorithm 2023/8

--- classics/algorithm/start4/start4.go
+++ classics/algorithm/start4/start4.go
@@ -3,12 +3,48 @@ package start4
 
 /*
 
-给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
 
-不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
 
+摩尔投票的方法
+1. >n/2 一定是众数
+2. 如果记众数的票数+1 非众数的票数-1 则一定所有数字的票数和 >0
+3. 若数组的前a个数字的票数和=0 则数组剩下(n-a)个数字的票数和一定仍 >0 即后(n -a) 个数字的众数仍为x
+
+
+空间复杂度 O(1)
+时间复杂度 O(n)
 */
 
-func removeDuplicates(nums []int) int {
+func majorityElement(nums []int) int {
+	var votes int
+	var d int
+	for _, num := range nums {
+		if votes == 0 {
+			d = num
+		}
 
+		if num == d {
+			votes++
+		} else {
+			votes--
+		}
+	}
+
+	/*
+	// 这部分用来判断是不是 大于 ⌊ n/2 ⌋ 的元素   这题是没必要
+	var count int
+	for _, num := range nums {
+		if num == d {
+			count++
+		}
+	}
+
+	if count > len(nums) {
+		return d
+	} else {
+		return 0
+	}*/
+	return d
 }
